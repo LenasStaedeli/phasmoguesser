04cff17975a9bbcace6b7b2ea38e4dd05c7df6b7
LenasStaedeli <lenas.staedeli@stud.kbw.ch>
2026-02-24 09:09:14 +0100

    replace loading image

--- src/Components/First_Screen.jsx
+++ src/Components/First_Screen.jsx
@@ -34,12 +34,11 @@ export default function First_Screen({
 
 
     return (
-        <>
-            <h1 hidden={hiddenpic === 1 ? false : true}>Welcome to Phasmo Guesser</h1>
+        <>            <h1 hidden={hiddenpic === 1 ? false : true}>Welcome to Phasmo Guesser</h1>
             <h2 hidden={hiddenpic === 1 ? false : true}>Click "map role" to get your spot</h2>
             <div>
                 <img
-                    src={`https://lenasstaedeli.github.io/phasmoguesser/Spots/${mapselector.map}/${mapselector.map}${mapselector.id}${mapselector.floor}.jpg`}
+                    src={`https://lenasstaedeli.github.io/phasmoguesser/Spots/${mapselector.map}/${mapselector.map}${mapselector.id}${mapselector.floor}.${mapselector === 0.5 ? `png` : `jpg`}`}
                     style={{height: "50%", width: "50%"}}
                     alt="Tanglewood"
                     hidden={hiddenpic}/>
