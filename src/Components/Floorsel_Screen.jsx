04f378b5b673e4a1862dff7280360328b6983c24
LenasStaedeli <lenas.staedeli@stud.kbw.ch>
2026-03-18 09:16:30 +0100

    finalize the floorselection code rework

--- src/Components/Floorsel_Screen.jsx
+++ src/Components/Floorsel_Screen.jsx
@@ -1,10 +1,11 @@
 import {useNavigate} from "react-router-dom";
 import data from "../data/spots.json";
 
-export default function Floorsel_Screen({setPoints, points, mapselect}) {
+export default function Testingground({mapselect, setPoints, points}){
     const navigate = useNavigate()
-    const mapselector = data[mapselect]
-
+    const mapselector = data[String(mapselect)]
+    const mapname = mapselector.map
+    const Mapfloors = data["Mapfloors"]
 
     function clickpic(floor) {
         if (mapselector.floor === floor) {
@@ -21,84 +22,106 @@ export default function Floorsel_Screen({setPoints, points, mapselect}) {
                 <h1>Floor Selection Screen</h1>
                 <button onClick={() => navigate("/Secondscreen")}>Back</button>
             </div>
-            {mapselector.map === "Tanglewood" && (
+            {Mapfloors[mapname].basement === true && (
                 <>
-                    <div>Basement</div>
                     <img
                         src={`https://lenasstaedeli.github.io/phasmoguesser/Floors/${mapselector.map}/${mapselector.map}basement.png`}
                         style={{height: "50%", width: "50%"}} alt="Tanglewood Basement"
                         onClick={() => clickpic("basement")}/>
                     <div>Basement</div>
-                    <img
-                        src={`https://lenasstaedeli.github.io/phasmoguesser/Floors/${mapselector.map}/${mapselector.map}first.png`}
-                        style={{height: "50%", width: "50%"}} alt="Tanglewood First" onClick={() => clickpic("first")}/>
-                    <div>First Floor</div>
                 </>
             )}
-            {mapselector.map === "Edgefield" && (
+            {Mapfloors[mapname].first === true && (
                 <>
                     <img
-                        src={`https://lenasstaedeli.github.io/phasmoguesser/Floors/${mapselector.map}/${mapselector.map}basement.png`}
-                        style={{height: "50%", width: "50%"}} alt="Edgefield Basement"
-                        onClick={() => clickpic("basement")}/>
-                    <div>Basement</div>
-                    <img
                         src={`https://lenasstaedeli.github.io/phasmoguesser/Floors/${mapselector.map}/${mapselector.map}first.png`}
-                        style={{height: "50%", width: "50%"}} alt="Edgefield First" onClick={() => clickpic("first")}/>
-                    <div>First Floor</div>
-                    <img
-                        src={`https://lenasstaedeli.github.io/phasmoguesser/Floors/${mapselector.map}/${mapselector.map}second.png`}
-                        style={{height: "50%", width: "50%"}} alt="Edgefield Second"
-                        onClick={() => clickpic("second")}/>
-                    <div>Second Floor</div>
-
+                        style={{height: "50%", width: "50%"}} alt="Tanglewood Basement"
+                        onClick={() => clickpic("first")}/>
+                    <div>first floor</div>
                 </>
             )}
+            {Mapfloors[mapname].second === true && (
+                <>
+                    <img
+                        src={`https://lenasstaedeli.github.io/phasmoguesser/Floors/${mapselector.map}/${mapselector.map}second.png`}
+                        style={{height: "50%", width: "50%"}} alt="Tanglewood Basement"
+                        onClick={() => clickpic("second")}/>
+                    <div>second floor</div>
+                </>
+            )}
+            {Mapfloors[mapname].third === true && (
+                <>
+                    <img
+                        src={`https://lenasstaedeli.github.io/phasmoguesser/Floors/${mapselector.map}/${mapselector.map}third.png`}
+                        style={{height: "50%", width: "50%"}} alt="Tanglewood Basement"
+                        onClick={() => clickpic("third")}/>
+                    <div>third floor</div>
+                </>
+            )}
+            {Mapfloors[mapname].fourth === true && (
+                <>
+                    <img
+                        src={`https://lenasstaedeli.github.io/phasmoguesser/Floors/${mapselector.map}/${mapselector.map}fourth.png`}
+                        style={{height: "50%", width: "50%"}} alt="Tanglewood Basement"
+                        onClick={() => clickpic("fourth")}/>
+                    <div>fourth floor</div>
+                </>
+            )}
+            {Mapfloors[mapname].fifth === true && (
+                <>
+                    <img
+                        src={`https://lenasstaedeli.github.io/phasmoguesser/Floors/${mapselector.map}/${mapselector.map}fifth.png`}
+                        style={{height: "50%", width: "50%"}} alt="Tanglewood Basement"
+                        onClick={() => clickpic("fifth")}/>
+                    <div>fifth floor</div>
+                </>
+            )}
+            {Mapfloors[mapname].sixth === true && (
+                <>
+                    <img
+                        src={`https://lenasstaedeli.github.io/phasmoguesser/Floors/${mapselector.map}/${mapselector.map}sixth.png`}
+                        style={{height: "50%", width: "50%"}} alt="Tanglewood Basement"
+                        onClick={() => clickpic("sixth")}/>
+                    <div>sixth floor</div>
+                </>
+            )}
+            {Mapfloors[mapname].seventh === true && (
+                <>
+                    <img
+                        src={`https://lenasstaedeli.github.io/phasmoguesser/Floors/${mapselector.map}/${mapselector.map}seventh.png`}
+                        style={{height: "50%", width: "50%"}} alt="Tanglewood Basement"
+                        onClick={() => clickpic("seventh")}/>
+                    <div>seventh floor</div>
+                </>
+            )}
+            {Mapfloors[mapname].eighth === true && (
+                <>
+                    <img
+                        src={`https://lenasstaedeli.github.io/phasmoguesser/Floors/${mapselector.map}/${mapselector.map}eighth.png`}
+                        style={{height: "50%", width: "50%"}} alt="Tanglewood Basement"
+                        onClick={() => clickpic("eighth")}/>
+                    <div>eighth floor</div>
+                </>
+            )}
+            {Mapfloors[mapname].ninth === true && (
+                <>
+                    <img
+                        src={`https://lenasstaedeli.github.io/phasmoguesser/Floors/${mapselector.map}/${mapselector.map}ninth.png`}
+                        style={{height: "50%", width: "50%"}} alt="Tanglewood Basement"
+                        onClick={() => clickpic("ninth")}/>
+                    <div>ninth floor</div>
+                </>
+            )}
+            {Mapfloors[mapname].tenth === true && (
+                <>
+                    <img
+                        src={`https://lenasstaedeli.github.io/phasmoguesser/Floors/${mapselector.map}/${mapselector.map}tenth.png`}
+                        style={{height: "50%", width: "50%"}} alt="Tanglewood Basement"
+                        onClick={() => clickpic("tenth")}/>
+                    <div>tenth floor</div>
+                </>
+            )}
+
         </>
     )
 }
-/*
-    const maps = [
-        "Tanglewood",
-        "Edgefield",
-        "Ridgeview",
-        "Willow",
-        "Pointhope",
-        "Gafton",
-        "Bleasdale",
-        "Prison",
-        "Higschool",
-        "Campwoodwind",
-        "Maplelodge",
-        "nellsdiner",
-        "Sunnymeadows"
-    ]
-    const floor = [
-      "basement"
-      "first"
-      "second"
-      "third"
-      "fourth"
-      "fifth"
-      "sixth"
-      "seventh"
-      "eighth"
-      "ninth"
-      "tenth"
-    ]
-    const number = [
-        []
-        []
-        []
-        []
-        []
-        []
-        []
-        []
-        []
-        []
-        []
-        []
-        []
-    ]
- */
